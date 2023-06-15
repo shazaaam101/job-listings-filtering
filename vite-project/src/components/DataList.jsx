c4f16b76d35fbb698ca90656b372990a4eda953d
Modx <122527984+shazaaam101@users.noreply.github.com>
2023-06-16 02:31:59 +0700

    Fix image not found

--- vite-project/src/components/DataList.jsx
+++ vite-project/src/components/DataList.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import DataItem from "./DataItem";
 import "../styles/dataList.css";
+import photosnapImg from "../images/photosnap.svg";
+import manageImg from "../images/manage.svg";
+import accountImg from "../images/account.svg";
+import eyecamImg from "../images/eyecam-co.svg";
+import faceitImg from "../images/faceit.svg";
+import insureImg from "../images/insure.svg";
+import loopStudiosImg from "../images/loop-studios.svg";
+import myhomeImg from "../images/myhome.svg";
+import shortlyImg from "../images/shortly.svg";
+import filterCompanyImg from "../images/the-air-filter-company.svg";
 
 const DataList = ({ myData }) => {
+  const imgArray = [
+    photosnapImg,
+    manageImg,
+    accountImg,
+    myhomeImg,
+    loopStudiosImg,
+    faceitImg,
+    shortlyImg,
+    insureImg,
+    eyecamImg,
+    filterCompanyImg,
+  ];
   return (
     <div className="data-list">
       {myData.map((data, idx) => (
         <DataItem
           key={idx}
           company={data.company}
-          logo={data.logo}
+          logo={imgArray?.at(idx)}
           newItem={data.new}
           featured={data.featured}
           position={data.position}
